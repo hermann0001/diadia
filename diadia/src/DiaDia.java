d59652cebcc5b3c5d3ad19a558097348818f4f33
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-27 20:09:13 +0200

    Add files via upload

---
+++ diadia/src/DiaDia.java
@@ -0,0 +1,114 @@
+
+
+import java.util.Scanner;
+
+/**
+ * Classe principale di diadia, un semplice gioco di ruolo ambientato al dia.
+ * Per giocare crea un'istanza di questa classe e invoca il letodo gioca
+ *
+ * Questa e' la classe principale crea e istanzia tutte le altre
+ *
+ * @author  docente di POO 
+ *         (da un'idea di Michael Kolling and David J. Barnes) 
+ *          
+ * @version base
+ */
+
+public class DiaDia {
+
+	static final private String MESSAGGIO_BENVENUTO = ""+
+			"Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n" +
+			"Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"+
+			"I locali sono popolati da strani personaggi, " +
+			"alcuni amici, altri... chissa!\n"+
+			"Ci sono attrezzi che potrebbero servirti nell'impresa:\n"+
+			"puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n" +
+			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
+			"Per conoscere le istruzioni usa il comando 'aiuto'.";
+	
+	static final private String[] elencoComandi = {"vai", "aiuto", "fine"};
+
+	private Partita partita;
+
+	public DiaDia() {
+		this.partita = new Partita();
+	}
+
+	public void gioca() {
+		String istruzione; 
+		Scanner scannerDiLinee;
+
+		System.out.println(MESSAGGIO_BENVENUTO);
+		scannerDiLinee = new Scanner(System.in);		
+		do		
+			istruzione = scannerDiLinee.nextLine();
+		while (!processaIstruzione(istruzione));
+	}   
+
+
+	/**
+	 * Processa una istruzione 
+	 *
+	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
+	 */
+	private boolean processaIstruzione(String istruzione) {
+		Comando comandoDaEseguire = new Comando(istruzione);
+
+		if (comandoDaEseguire.getNome().equals("fine")) {
+			this.fine(); 
+			return true;
+		} else if (comandoDaEseguire.getNome().equals("vai"))
+			this.vai(comandoDaEseguire.getParametro());
+		else if (comandoDaEseguire.getNome().equals("aiuto"))
+			this.aiuto();
+		else
+			System.out.println("Comando sconosciuto");
+		if (this.partita.vinta()) {
+			System.out.println("Hai vinto!");
+			return true;
+		} else
+			return false;
+	}   
+
+	// implementazioni dei comandi dell'utente:
+
+	/**
+	 * Stampa informazioni di aiuto.
+	 */
+	private void aiuto() {
+		for(int i=0; i< elencoComandi.length; i++) 
+			System.out.print(elencoComandi[i]+" ");
+		System.out.println();
+	}
+
+	/**
+	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra 
+	 * e ne stampa il nome, altrimenti stampa un messaggio di errore
+	 */
+	private void vai(String direzione) {
+		if(direzione==null)
+			System.out.println("Dove vuoi andare ?");
+		Stanza prossimaStanza = null;
+		prossimaStanza = this.partita.getStanzaCorrente().getStanzaAdiacente(direzione);
+		if (prossimaStanza == null)
+			System.out.println("Direzione inesistente");
+		else {
+			this.partita.setStanzaCorrente(prossimaStanza);
+			int cfu = this.partita.getCfu();
+			this.partita.setCfu(cfu--);
+		}
+		System.out.println(partita.getStanzaCorrente().getDescrizione());
+	}
+
+	/**
+	 * Comando "Fine".
+	 */
+	private void fine() {
+		System.out.println("Grazie di aver giocato!");  // si desidera smettere
+	}
+
+	public static void main(String[] argc) {
+		DiaDia gioco = new DiaDia();
+		gioco.gioca();
+	}
+}
---
+++ diadia/src/Partita.java
@@ -0,0 +1,109 @@
+
+
+
+/**
+ * Questa classe modella una partita del gioco
+ *
+ * @author  docente di POO
+ * @see Stanza
+ * @version base
+ */
+
+public class Partita {
+
+	static final private int CFU_INIZIALI = 20;
+
+	private Stanza stanzaCorrente;
+	private Stanza stanzaVincente;
+	private boolean finita;
+	private int cfu;
+	
+	public Partita(){
+		creaStanze();
+		this.finita = false;
+		this.cfu = CFU_INIZIALI;
+	}
+
+    /**
+     * Crea tutte le stanze e le porte di collegamento
+     */
+    private void creaStanze() {
+
+		/* crea gli attrezzi */
+    	Attrezzo lanterna = new Attrezzo("lanterna",3);
+		Attrezzo osso = new Attrezzo("osso",1);
+    	
+		/* crea stanze del labirinto */
+		Stanza atrio = new Stanza("Atrio");
+		Stanza aulaN11 = new Stanza("Aula N11");
+		Stanza aulaN10 = new Stanza("Aula N10");
+		Stanza laboratorio = new Stanza("Laboratorio Campus");
+		Stanza biblioteca = new Stanza("Biblioteca");
+		
+		/* collega le stanze */
+		atrio.impostaStanzaAdiacente("nord", biblioteca);
+		atrio.impostaStanzaAdiacente("est", aulaN11);
+		atrio.impostaStanzaAdiacente("sud", aulaN10);
+		atrio.impostaStanzaAdiacente("ovest", laboratorio);
+		aulaN11.impostaStanzaAdiacente("est", laboratorio);
+		aulaN11.impostaStanzaAdiacente("ovest", atrio);
+		aulaN10.impostaStanzaAdiacente("nord", atrio);
+		aulaN10.impostaStanzaAdiacente("est", aulaN11);
+		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
+		laboratorio.impostaStanzaAdiacente("est", atrio);
+		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
+		biblioteca.impostaStanzaAdiacente("sud", atrio);
+
+        /* pone gli attrezzi nelle stanze */
+		aulaN10.addAttrezzo(lanterna);
+		atrio.addAttrezzo(osso);
+
+		// il gioco comincia nell'atrio
+        stanzaCorrente = atrio;  
+		stanzaVincente = biblioteca;
+    }
+
+	public Stanza getStanzaVincente() {
+		return stanzaVincente;
+	}
+
+	public void setStanzaCorrente(Stanza stanzaCorrente) {
+		this.stanzaCorrente = stanzaCorrente;
+	}
+
+	public Stanza getStanzaCorrente() {
+		return this.stanzaCorrente;
+	}
+	
+	/**
+	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * @return vero se partita vinta
+	 */
+	public boolean vinta() {
+		return this.getStanzaCorrente()== this.getStanzaVincente();
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' finita
+	 * @return vero se partita finita
+	 */
+	public boolean isFinita() {
+		return finita || vinta() || (cfu == 0);
+	}
+
+	/**
+	 * Imposta la partita come finita
+	 *
+	 */
+	public void setFinita() {
+		this.finita = true;
+	}
+
+	public int getCfu() {
+		return this.cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;		
+	}	
+}
