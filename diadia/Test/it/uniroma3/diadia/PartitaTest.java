8d92444b321c7dda3a13c205177ac1fb53e438e8
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-28 19:19:34 +0200

    test per la classe partita

---
+++ diadia/Test/it/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,54 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartitaTest {
+
+	@Before
+	public void setUp(){
+	}
+
+	@Test
+	public void testGetStanzaVincenteSenzaStanzaVincente() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetStanzaVincente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStanzaCorrenteSenzaStanze() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetStanzaCorrente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVintaSenzaStanzaVincente() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testVintaSenzaStanze() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVinta() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIsFinita() {
+		fail("Not yet implemented");
+	}
+
+}
