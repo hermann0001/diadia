c54cddb9c14d24f6430727330c114af3aa290186
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-28 23:47:16 +0200

    Aggiunto test per la classe Giocatore

---
+++ diadia/Test/it/uniroma3/diadia/GiocatoreTest.java
@@ -0,0 +1,27 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GiocatoreTest {
+	private Giocatore giocatore;
+
+	@Before
+	public void setUp(){
+		giocatore = new Giocatore("Player Test");
+	}
+
+	@Test
+	public void testGetCfu() {
+		assertEquals(20, this.giocatore.getCfu());
+	}
+
+	@Test
+	public void testSetCfu() {
+		this.giocatore.setCfu(12);
+		assertEquals(12, this.giocatore.getCfu());
+	}
+
+}
