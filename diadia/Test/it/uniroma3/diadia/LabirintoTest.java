3da86b29fce0495049b2d5fbb07e52eeba083b00
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:42:27 +0200

    Add files via upload

---
+++ diadia/Test/it/uniroma3/diadia/LabirintoTest.java
@@ -0,0 +1,43 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LabirintoTest {
+
+	private Labirinto labirintoTest;
+	private Stanza stanzaFinale;
+	private Stanza stanzaIniziale;
+
+	@Before
+	public void setUp() {
+		this.stanzaFinale = new Stanza("Test1");
+		this.stanzaIniziale = new Stanza("Test2");
+		this.labirintoTest = new Labirinto("MappaTest");
+	}
+
+	/* Testo solo i metodi getter, testando cosi implicitamente anche i setter */
+
+	/*
+	 * Testo se la stanza finale ritornata da getStanzaFinale() sia quella appena
+	 * settata tramite il metodo setStanzaFinale()
+	 */
+	@Test
+	public void testGetStanzaFinale() {
+		this.labirintoTest.setStanzaFinale(stanzaFinale);
+		assertEquals(stanzaFinale, this.labirintoTest.getStanzaFinale());
+	}
+
+	/*
+	 * Testo se la stanza iniziale ritornata da getStanzaIniziale() sia quella
+	 * appena settata tramite il metodo setStanzaIniziale()
+	 */
+	@Test
+	public void testGetStanzaIniziale() {
+		this.labirintoTest.setStanzaIniziale(stanzaIniziale);
+		assertEquals(stanzaIniziale, this.labirintoTest.getStanzaIniziale());
+	}
+
+}
