958c39e1c28a4dd561dd3e3891eaf8134d27eedb
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:28:13 +0200

    RImozione di alcuni metodi
    Avendo creato la classe labirinto, ho rimosso alcune responsabilità dalla classe partita. Inoltre ho aggiunto il javadoc e alcuni metodi

--- diadia/src/it/uniroma3/diadia/Partita.java
+++ diadia/src/it/uniroma3/diadia/Partita.java
@@ -1,90 +1,76 @@
 package it.uniroma3.diadia;
 
+import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
 /**
  * Questa classe modella una partita del gioco
  *
- * @author  docente di POO
+ * @author docente di POO
  * @see Stanza
  * @version base
  */
 
 public class Partita {
+	private Labirinto labirinto;
 	private Stanza stanzaCorrente;
-	private Stanza stanzaVincente;
 	private boolean finita;
 	private Giocatore giocatore;
-	
-	public Partita(){
-		this.giocatore = new Giocatore("Player 1");   //TODO inserire nome da tastiera
-		creaStanze();
+
+	public Partita() {
+		this.giocatore = new Giocatore("Player 1"); // TODO inserire nome da tastiera
+		this.labirinto = new Labirinto("Mappa 1"); // TODO inserire nome da tastiera
 		this.finita = false;
+		this.stanzaCorrente = this.labirinto.getStanzaIniziale();
 	}
 
-    /**
-     * Crea tutte le stanze e le porte di collegamento
-     */
-    private void creaStanze() {
-
-		/* crea gli attrezzi */
-    	Attrezzo lanterna = new Attrezzo("lanterna",3);
-		Attrezzo osso = new Attrezzo("osso",1);
-    	
-		/* crea stanze del labirinto */
-		Stanza atrio = new Stanza("Atrio");
-		Stanza aulaN11 = new Stanza("Aula N11");
-		Stanza aulaN10 = new Stanza("Aula N10");
-		Stanza laboratorio = new Stanza("Laboratorio Campus");
-		Stanza biblioteca = new Stanza("Biblioteca");
-		
-		/* collega le stanze */
-		atrio.impostaStanzaAdiacente("nord", biblioteca);
-		atrio.impostaStanzaAdiacente("est", aulaN11);
-		atrio.impostaStanzaAdiacente("sud", aulaN10);
-		atrio.impostaStanzaAdiacente("ovest", laboratorio);
-		aulaN11.impostaStanzaAdiacente("est", laboratorio);
-		aulaN11.impostaStanzaAdiacente("ovest", atrio);
-		aulaN10.impostaStanzaAdiacente("nord", atrio);
-		aulaN10.impostaStanzaAdiacente("est", aulaN11);
-		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
-		laboratorio.impostaStanzaAdiacente("est", atrio);
-		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
-		biblioteca.impostaStanzaAdiacente("sud", atrio);
-
-        /* pone gli attrezzi nelle stanze */
-		aulaN10.addAttrezzo(lanterna);
-		atrio.addAttrezzo(osso);
-
-		// il gioco comincia nell'atrio
-        stanzaCorrente = atrio;  
-		stanzaVincente = biblioteca;
-    }
-
-	public Stanza getStanzaVincente() {
-		return stanzaVincente;
-	}
-
+	/**
+	 * Imposta stanzaCorrente come stanza corrente
+	 * 
+	 * @param stanzaCorrente
+	 */
 	public void setStanzaCorrente(Stanza stanzaCorrente) {
 		this.stanzaCorrente = stanzaCorrente;
 	}
 
+	/**
+	 * Ritorna la stanzaCorrente
+	 * 
+	 * @return Stanza
+	 */
 	public Stanza getStanzaCorrente() {
 		return this.stanzaCorrente;
 	}
-	
+
+	/**
+	 * Ritorna il labirinto attuale della partita
+	 * 
+	 * @return Labirinto
+	 */
+	public Labirinto getLabirinto() {
+		return this.labirinto;
+	}
+
 	/**
 	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * 
 	 * @return vero se partita vinta
 	 */
 	public boolean vinta() {
-		return this.getStanzaCorrente()== this.getStanzaVincente();
+		if (this.labirinto.getStanzaFinale() == null)
+			return false;
+
+		return this.getStanzaCorrente() == this.labirinto.getStanzaFinale();
 	}
 
 	/**
 	 * Restituisce vero se e solo se la partita e' finita
+	 * 
 	 * @return vero se partita finita
 	 */
 	public boolean isFinita() {
-		return finita || vinta() || (this.giocatore.getCfu() == 0);
+		return this.finita || this.vinta() || (this.giocatore.getCfu() == 0);
 	}
 
 	/**
@@ -94,18 +80,14 @@ public class Partita {
 	public void setFinita() {
 		this.finita = true;
 	}
-	
-	/*
-	 * Questi metodi sono necessari se si vuole accedere ai cfu da un oggetto
-	 * istanza partita, rispettando lo scope di giocatore
+
+	/**
+	 * Ritorna l'attuale giocatore
+	 * 
+	 * @return Giocatore
 	 */
-	public int getCfu() {
-		return this.giocatore.getCfu();
+	public Giocatore getGiocatore() {
+		return this.giocatore;
 	}
-
-	public void setCfu(int cfu) {
-		this.giocatore.setCfu(cfu);		
-	}	
-
 
 }
