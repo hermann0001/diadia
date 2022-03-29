20e642cc5155a304d794cb3db41e4405de04fdf2
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:22:27 +0200

    Indentazione

--- diadia/src/it/uniroma3/diadia/Comando.java
+++ diadia/src/it/uniroma3/diadia/Comando.java
@@ -3,43 +3,41 @@ package it.uniroma3.diadia;
 import java.util.Scanner;
 
 /**
- * Questa classe modella un comando.
- * Un comando consiste al piu' di due parole:
- * il nome del comando ed un parametro
- * su cui si applica il comando.
- * (Ad es. alla riga digitata dall'utente "vai nord"
- *  corrisponde un comando di nome "vai" e parametro "nord").
+ * Questa classe modella un comando. Un comando consiste al piu' di due parole:
+ * il nome del comando ed un parametro su cui si applica il comando. (Ad es.
+ * alla riga digitata dall'utente "vai nord" corrisponde un comando di nome
+ * "vai" e parametro "nord").
  *
- * @author  docente di POO
+ * @author docente di POO
  * @version base
  */
 
 public class Comando {
 
-    private String nome;
-    private String parametro;
+	private String nome;
+	private String parametro;
 
-    public Comando(String istruzione) {
+	public Comando(String istruzione) {
 		Scanner scannerDiParole = new Scanner(istruzione);
 
 		// prima parola: nome del comando
 		if (scannerDiParole.hasNext())
-			this.nome = scannerDiParole.next(); 
+			this.nome = scannerDiParole.next();
 
 		// seconda parola: eventuale parametro
 		if (scannerDiParole.hasNext())
 			this.parametro = scannerDiParole.next();
-    }
+	}
 
-    public String getNome() {
-        return this.nome;
-    }
+	public String getNome() {
+		return this.nome;
+	}
 
-    public String getParametro() {
-        return this.parametro;
-    }
+	public String getParametro() {
+		return this.parametro;
+	}
 
-    public boolean sconosciuto() {
-        return (this.nome == null);
-    }
+	public boolean sconosciuto() {
+		return (this.nome == null);
+	}
 }
