e485d1546f1b7e10ae345969e4bff06acfb89794
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:25:37 +0200

    Aggiunto javadoc

--- diadia/src/it/uniroma3/diadia/Giocatore.java
+++ diadia/src/it/uniroma3/diadia/Giocatore.java
@@ -1,38 +1,49 @@
-package it.uniroma3.diadia;
+package it.uniroma3.diadia.giocatore;
+
+/**
+ * Questa classe modella un giocatore
+ *
+ * @author Hermann Tamilia
+ * @see Borsa
+ * @version base
+ */
 
 public class Giocatore {
 	private String nome;
 	private int cfu;
 	private Borsa borsa;
 	static final private int CFU_INIZIALI = 20;
-	
+
 	public Giocatore(String nome) {
 		this.nome = nome;
 		this.borsa = new Borsa();
 		this.cfu = CFU_INIZIALI;
 	}
-	
-	public boolean addAttrezzo(Attrezzo attrezzo) {
-		return this.borsa.addAttrezzo(attrezzo);
+
+	/**
+	 * Ritorna l'attuale borsa
+	 * 
+	 * @return Borsa
+	 */
+	public Borsa getBorsa() {
+		return this.borsa;
 	}
-	
-	public Attrezzo removeAttrezzo(String nomeAttrezzo) {
-		return this.borsa.removeAttrezzo(nomeAttrezzo);
-	}
-	
-	public Attrezzo getAttrezzo(String nomeAttrezzo) {
-		return this.borsa.getAttrezzo(nomeAttrezzo);
-	}
-	
-	public boolean hasAttrezzo(String nomeAttrezzo) {
-		return this.borsa.hasAttrezzo(nomeAttrezzo);
-	}
-	
+
+	/**
+	 * Ritorna il numero di cfu del giocatore
+	 * 
+	 * @return int
+	 */
 	public int getCfu() {
 		return this.cfu;
 	}
 
+	/**
+	 * Imposta il numero di cfu del giocatore
+	 * 
+	 * @param cfu
+	 */
 	public void setCfu(int cfu) {
-		this.cfu = cfu;		
-	}	
+		this.cfu = cfu;
+	}
 }
