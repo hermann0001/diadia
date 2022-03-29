c3a285ea8906e97aa41da90ec002a17aeebbacc8
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:37:21 +0200

    Aggiunti i package

--- diadia/Test/it/uniroma3/diadia/BorsaTest.java
+++ diadia/Test/it/uniroma3/diadia/BorsaTest.java
@@ -1,9 +1,11 @@
-package it.uniroma3.diadia;
+package it.uniroma3.diadia.giocatore;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 public class BorsaTest {
 
