f605ba189078e352aa86181664ff4b072eeaade4
Hermann <her.tamilia@stud.uniroma3.it>
2022-03-30 00:20:28 +0200

    Indentazione

--- diadia/src/it/uniroma3/diadia/Attrezzo.java
+++ diadia/src/it/uniroma3/diadia/Attrezzo.java
@@ -1,13 +1,10 @@
-package it.uniroma3.diadia;
-
+package it.uniroma3.diadia.attrezzi;
 
 /**
- * Una semplice classe che modella un attrezzo.
- * Gli attrezzi possono trovarsi all'interno delle stanze
- * del labirinto.
- * Ogni attrezzo ha un nome ed un peso.
+ * Una semplice classe che modella un attrezzo. Gli attrezzi possono trovarsi
+ * all'interno delle stanze del labirinto. Ogni attrezzo ha un nome ed un peso.
  *
- * @author  docente di POO
+ * @author docente di POO
  * @see Stanza
  * @version base
  */
@@ -18,6 +15,7 @@ public class Attrezzo {
 
 	/**
 	 * Crea un attrezzo
+	 * 
 	 * @param nome il nome che identifica l'attrezzo
 	 * @param peso il peso dell'attrezzo
 	 */
@@ -28,6 +26,7 @@ public class Attrezzo {
 
 	/**
 	 * Restituisce il nome identificatore dell'attrezzo
+	 * 
 	 * @return il nome identificatore dell'attrezzo
 	 */
 	public String getNome() {
@@ -36,6 +35,7 @@ public class Attrezzo {
 
 	/**
 	 * Restituisce il peso dell'attrezzo
+	 * 
 	 * @return il peso dell'attrezzo
 	 */
 	public int getPeso() {
@@ -44,10 +44,11 @@ public class Attrezzo {
 
 	/**
 	 * Restituisce una rappresentazione stringa di questo attrezzo
+	 * 
 	 * @return la rappresentazione stringa
 	 */
 	public String toString() {
-		return this.getNome()+" ("+this.getPeso()+"kg)";
+		return this.getNome() + " (" + this.getPeso() + "kg)";
 	}
 
 }
