25f55b6b6e2ff9d350ffb645e4d26760bf9e91af
hermann <hermann@kali.lan>
2022-04-20 21:45:44 +0200

    implementazione IOSimulator

---
+++ diadia/src/it/uniroma3/diadia/IOSimulator.java
@@ -0,0 +1,17 @@
+package it.uniroma3.diadia;
+
+public class IOSimulator implements IO{
+
+	@Override
+	public void mostraMessaggio(String messaggio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String leggiRiga() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
