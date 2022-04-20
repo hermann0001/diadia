e1b47f23e7a2d9da9a34db5c46253b73dfadd5d8
hermann <hermann@kali.lan>
2022-04-20 12:28:31 +0200

    fix test borsa

--- diadia/Test/it/uniroma3/diadia/comando/ComandoPosaTest.java
+++ diadia/Test/it/uniroma3/diadia/comando/ComandoPosaTest.java
@@ -12,9 +12,9 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 import it.uniroma3.diadia.giocatore.Borsa;
 
 public class ComandoPosaTest {
-	private static final int MAX_NUM_ATTREZZI = 10;
+	private static final int MAX_PESO = 10;
 	private Stanza stanza;
-	private Attrezzo attrezzi[];
+	private Attrezzo attrezzo;
 	private ComandoPosa comando;
 	private Partita partita;
 	private IOConsole io;
@@ -25,7 +25,6 @@ public class ComandoPosaTest {
 		this.io = new IOConsole();
 		this.comando = new ComandoPosa();
 		this.stanza = new Stanza("Test");
-		this.attrezzi = new Attrezzo[MAX_NUM_ATTREZZI];
 		this.partita = new Partita(this.io);
 		this.partita.setStanzaCorrente(this.stanza);
 		this.borsa = this.partita.getGiocatore().getBorsa();
@@ -33,8 +32,8 @@ public class ComandoPosaTest {
 
 	@Test
 	public void testPosaUnSoloAttrezzo() {
-		this.attrezzi[0] = new Attrezzo("spada", 2);
-		this.borsa.addAttrezzo(this.attrezzi[0]);
+		this.attrezzo = new Attrezzo("spada", 2);
+		this.borsa.addAttrezzo(this.attrezzo);
 		this.comando.setParametro("spada");
 		this.comando.esegui(this.partita);
 		assertFalse(this.partita.getGiocatore().getBorsa().hasAttrezzo("spada"));
@@ -43,27 +42,21 @@ public class ComandoPosaTest {
 	
 	@Test
 	public void testPosaAttrezzoNullo() {
-		this.attrezzi[0] = null;
-		this.partita.setStanzaCorrente(this.stanza);
+		this.attrezzo = null;
 		this.comando.setParametro("spada");
 		this.comando.esegui(this.partita);
-		this.borsa.addAttrezzo(this.attrezzi[0]);
+		this.borsa.addAttrezzo(this.attrezzo);
 		assertFalse(this.partita.getGiocatore().getBorsa().hasAttrezzo("spada"));
 		assertFalse(this.partita.getStanzaCorrente().hasAttrezzo("spada"));
 	}
 	
 	@Test
 	public void testPrendiAttrezzoBorsaPiena() {
-		for(int i = 0; i < MAX_NUM_ATTREZZI; i++){
-			this.attrezzi[i] = new Attrezzo("Attrezzo"+(i+1),0);
-			this.stanza.addAttrezzo(this.attrezzi[i]);
-			}
-		Attrezzo overflow = new Attrezzo("overflow", 0);
-		this.partita.setStanzaCorrente(this.stanza);
-		this.comando.setParametro("overflow");
-		this.stanza.addAttrezzo(overflow);
+		this.attrezzo = new Attrezzo("martello", MAX_PESO+1);
+		this.comando.setParametro("martello");
+		this.borsa.addAttrezzo(attrezzo);
 		this.comando.esegui(this.partita);
-		assertFalse(this.partita.getGiocatore().getBorsa().hasAttrezzo("overflow"));
-		assertFalse(this.partita.getStanzaCorrente().hasAttrezzo("overflow"));
+		assertFalse(this.partita.getGiocatore().getBorsa().hasAttrezzo("martello"));
+		assertFalse(this.partita.getStanzaCorrente().hasAttrezzo("martello"));
 	}
 }
