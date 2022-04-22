fbae0fdf2b8750427addd2fec295d0b32bf0f633
hermann <hermann@kali.lan>
2022-04-22 20:32:21 +0200

    implementata la classe iosimulator che simulerà una partita in automatico

--- diadia/src/it/uniroma3/diadia/IOSimulator.java
+++ diadia/src/it/uniroma3/diadia/IOSimulator.java
@@ -1,17 +1,41 @@
 package it.uniroma3.diadia;
 
 public class IOSimulator implements IO {
+	
+	private String righeDaLeggere[];
+	private String messaggiProdotti[];
+	private int indiceRigheDaLeggere;
+	private int indiceMessaggiProdotti;
+	private int indiceMessaggiStampati;
 
+	public IOSimulator(String... righeDaLeggere) {
+		this.indiceRigheDaLeggere = 0;
+		this.righeDaLeggere = righeDaLeggere;
+		this.messaggiProdotti = new String[100];
+		this.indiceMessaggiProdotti = 0;
+		this.indiceMessaggiStampati = 0;
+	}
+	
 	@Override
 	public void mostraMessaggio(String messaggio) {
-		// TODO Auto-generated method stub
-		
+		this.messaggiProdotti[this.indiceMessaggiProdotti] = messaggio;
+		this.indiceMessaggiProdotti++;
 	}
 
 	@Override
 	public String leggiRiga() {
-		// TODO Auto-generated method stub
-		return null;
+		String rigaLetta = this.righeDaLeggere[this.indiceRigheDaLeggere];
+		this.indiceRigheDaLeggere++;
+		return rigaLetta;
 	}
 	
+	public String nextMessaggio() {
+		String next = this.messaggiProdotti[this.indiceMessaggiStampati];
+		this.indiceMessaggiStampati++;
+		return next;
+	}
+	
+	public boolean hasNextMessaggio() {
+		return this.indiceMessaggiStampati < this.indiceMessaggiProdotti;
+	}
 }
