e0ee531d1e19baea463a7bd83b03bb1b7dab5d6a
hermann <hermann@kali.lan>
2022-06-06 01:22:10 +0200

    ora comando guarda stampa anche eventuali personaggi

--- diadia/src/it/uniroma3/diadia/comando/ComandoGuarda.java
+++ diadia/src/it/uniroma3/diadia/comando/ComandoGuarda.java
@@ -3,6 +3,7 @@ package it.uniroma3.diadia.comando;
 import it.uniroma3.diadia.Partita;
 import it.uniroma3.diadia.ambienti.Stanza;
 import it.uniroma3.diadia.giocatore.Borsa;
+import it.uniroma3.diadia.personaggi.AbstractPersonaggio;
 
 /**
  * Comando: guarda
@@ -24,11 +25,18 @@ public class ComandoGuarda extends AbstractComando {
 		this.io = partita.getIoconsole();
 
 		
-		Stanza stanzaCorrente = partita.getStanzaCorrente();
-		Borsa borsaCorrente = partita.getGiocatore().getBorsa();
+		final Stanza stanzaCorrente = partita.getStanzaCorrente();
+		final Borsa borsaCorrente = partita.getGiocatore().getBorsa();
+		final AbstractPersonaggio personaggio = stanzaCorrente.getPersonaggio();
+
 
 		this.io.mostraMessaggio(stanzaCorrente.getDescrizione());
 		this.io.mostraMessaggio(borsaCorrente.toString());
+		
+		if(personaggio != null) {
+			this.io.mostraMessaggio(personaggio.getClass().toString() + " " + personaggio.getNome());
+		}
+		
 		this.io.mostraMessaggio("Mappa:" + partita.getLabirinto().getNome());
 		this.io.mostraMessaggio("Giocatore:" + partita.getGiocatore().getNome());
 		this.io.mostraMessaggio("CFU:" + partita.getGiocatore().getCfu());
