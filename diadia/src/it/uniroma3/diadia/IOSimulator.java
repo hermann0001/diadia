5e2b7b688e89c64dfa3343d35e7246f09e48a9cf
hermann <hermann@kali.lan>
2022-06-06 22:34:31 +0200

    i messaggi prodotti da iosimulator sono gestiti da una mappa <integer, list<string>>

--- diadia/src/it/uniroma3/diadia/IOSimulator.java
+++ diadia/src/it/uniroma3/diadia/IOSimulator.java
@@ -13,32 +13,34 @@ import java.util.*;
 
 public class IOSimulator implements IO {
 
-	private List<String> righeDaLeggere;		//la lista di righe da leggere
-	private List<String> messaggiProdotti;		//la lista di messagi prodotti dal gioco
-	private int indiceRigheLette;			//un indice per contare quanti comandi sono stati letti
-	private int indiceMessaggiProdotti;		//un indice per contare quanti messaggi sono stati prodotti dal gioco
-	private int indiceMessaggiStampati;		//un indice per contare quanti messaggi sono stati "visti" / "visitati"
+	private List<String> righeDaLeggere;						//la lista di righe da leggere
+	private int indiceRigheDaLeggere;							//un indice per contare quanti comandi sono stati letti
+	private Map<Integer, List<String>> indice2messaggiProdotti;
+	private int ultimoIndiceMappaMostrato;
+	private int ultimoIndiceListaMostrato;
 
 	/**
 	 * istanzia due linkedList e imposta gli indici a 0
 	 */
 	public IOSimulator() {
 		this.righeDaLeggere = new LinkedList<>();
-		this.messaggiProdotti = new LinkedList<>();
-		this.indiceRigheLette = 0;
-		this.indiceMessaggiProdotti = 0;
-		this.indiceMessaggiStampati = 0;
+		this.indice2messaggiProdotti = new HashMap<Integer, List<String>>();
+		this.indiceRigheDaLeggere = 0;
+		this.ultimoIndiceMappaMostrato = 0;
+		this.ultimoIndiceListaMostrato = 0;
 	}
 	
 	/**
-	 * Salva i messaggi prodotti nella lista e incrementa il contatore
+	 * Salva i messaggi prodotti nella lista e la aggiunge alla mappa
 	 * 
 	 * @param messaggio messaggio prodotto
 	 */
 	@Override
 	public void mostraMessaggio(String messaggio) {
-		this.messaggiProdotti.add(messaggio);
-		this.indiceMessaggiProdotti++;
+		if(!this.indice2messaggiProdotti.containsKey(this.indiceRigheDaLeggere))
+			this.indice2messaggiProdotti.put(this.indiceRigheDaLeggere, new LinkedList<String>());
+		List<String> l = this.indice2messaggiProdotti.get(this.indiceRigheDaLeggere);
+		l.add(messaggio);
 	}
 	
 	/**
@@ -49,8 +51,8 @@ public class IOSimulator implements IO {
 	 */
 	@Override
 	public String leggiRiga() {
-		String rigaLetta = this.righeDaLeggere.get(this.indiceRigheLette);
-		this.indiceRigheLette++;
+		String rigaLetta = this.righeDaLeggere.get(this.indiceRigheDaLeggere);
+		this.indiceRigheDaLeggere++;
 		return rigaLetta;
 	}
 
@@ -69,15 +71,25 @@ public class IOSimulator implements IO {
 	 * @return messaggio i-esimo
 	 */
 	public String nextMessaggio() {
-		String next = this.messaggiProdotti.get(this.indiceMessaggiStampati);
-		this.indiceMessaggiStampati++;
-		return next;
+		List<String> messaggiDaMostrare = this.indice2messaggiProdotti.get(this.ultimoIndiceMappaMostrato);
+		if(this.ultimoIndiceListaMostrato < messaggiDaMostrare.size()) {
+			String messaggio = messaggiDaMostrare.get(this.ultimoIndiceListaMostrato);
+			this.ultimoIndiceListaMostrato++;
+			return messaggio;
+		}
+		this.ultimoIndiceListaMostrato = 0;
+		this.ultimoIndiceMappaMostrato++;
+		return this.nextMessaggio();
 	}
 	
 	/**
 	 * @return true se ci sono ancora messaggi prodotti, false altrimenti
 	 */
 	public boolean hasNextMessaggio() {
-		return this.indiceMessaggiStampati < this.indiceMessaggiProdotti;
+		List<String> messaggiDaMostrare = this.indice2messaggiProdotti.get(this.ultimoIndiceMappaMostrato);
+		if(this.ultimoIndiceListaMostrato < messaggiDaMostrare.size()) 
+			return true;
+		else
+			return this.indice2messaggiProdotti.containsKey(this.ultimoIndiceMappaMostrato +1);
 	}
 }
